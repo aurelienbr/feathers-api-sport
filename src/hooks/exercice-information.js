7cddbb83a108685b50e9cd30bf6618e35025a1b7
Guillaume Moreau <guillaume.moreau@viacesi.fr>
2018-02-28 18:43:41 +0100

    Exercices

--- src/hooks/exercice-information.js
+++ src/hooks/exercice-information.js
@@ -33,7 +33,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     var description = '';
     var video = '';
     var share = '';
-    var ownerField = context.params.user._id;
+    var ownerId = context.params.user._id;
     //var official ='';
 
     if(data.secondaryMuscularGroup){
@@ -61,7 +61,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     }
 
     context.data = {
-      ownerField,
+      ownerId,
       name,
       image,
       principalMuscularGroup,
