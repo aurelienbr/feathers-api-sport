ecea533c5d5ab04711c724e0d57382ac585f4b80
aurelienbr <aurelien.brachet@viacesi.fr>
2018-03-03 13:20:45 +0100

    commit session

--- src/services/sessions/sessions.hooks.js
+++ src/services/sessions/sessions.hooks.js
@@ -1,12 +1,16 @@
+const sessionInformation = require('../../hooks/session-information.js');
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const hooks = require('feathers-authentication-hooks');
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [authenticate('jwt')],
+    update: [authenticate('jwt')],
+    patch: [authenticate('jwt')],
+    remove: [hooks.restrictToOwner({ idField: 'id', ownerField: 'sentBy' })]
   },
 
   after: {
