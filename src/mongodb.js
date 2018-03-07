8bf7da2c4985e74591a8ace2756c7280957cd345
aurelienbr <aurelien.brachet@viacesi.fr>
2018-03-07 17:54:41 +0100

    test file env

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,9 +2,8 @@ const url = require('url');
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function(app) {
-  const config = app.get('mongodb');
   const dbName = url.parse(config).path.substring(1);
-  const promise = MongoClient.connect(config).then(client => {
+  const promise = MongoClient.connect(process.env.MONGODB_URI).then(client => {
     // For mongodb <= 2.2
     if (client.collection) {
       return client;
